d4c1d6910ed046f5e66ae0ed5246a7e7c2dafdb1
dev-raimundos <devraimundos@outlook.com>
2025-06-21 13:11:42 -0300

    FEAT: New DB

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using ApiIdentityEndPoint.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiIdentityEndPoint.Data;
+
+public class AppDbContext(DbContextOptions options) : IdentityDbContext<User>(options)
+{
+    public string Document { get; set; } = string.Empty;
+}
---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ApiIdentityEndPoint.Models;
+
+public class User : IdentityUser
+{
+    
+}
